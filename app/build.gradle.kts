4aed8da64695b3152a0a97012c5ecc9f0cceef21
JoseRoyo <joseroyo3@hotmail.com>
2025-01-11 13:15:21 +0100

    Ejercicio 6. Añadiendo contenido a la pantalla

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.campusdigitalfp.filmoteca"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.campusdigitalfp.filmoteca"
--- app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
@@ -1,47 +1,75 @@
 package com.campusdigitalfp.filmoteca
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            FilmotecaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AboutScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FilmotecaTheme {
-        Greeting("Android")
+fun AboutScreen() {
+
+    val context = LocalContext.current
+
+
+    Column(
+        modifier = Modifier.padding(16.dp).fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement  = Arrangement.Center
+    ) {
+        Text(text = "Creada por TuNombre")
+
+        Image(
+            painter = painterResource(id = R.drawable.perfil),
+            contentDescription = "Foto perfil",
+            Modifier.size(50.dp)
+        )
+
+        Row() {
+            Button(onClick = {showToast(context= context, message = "Funcionalidad sin implementar")}) {
+                Text(text = "Ir al sitio web")
+            }
+            Button(onClick = { showToast(context, "Funcionalidad sin implementar") }) {
+                Text(text = "Obtener soporte")
+            }
+        }
+
+        Button(onClick = { showToast(context,"Funcionalidad sin implementar") }) {
+            Text(text = "Volver")
+        }
     }
 }
+
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
