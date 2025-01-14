e246e91f8e3406ceb7d70ed5115452aa6d97edd2
JoseRoyo <joseroyo3@hotmail.com>
2025-01-14 16:26:45 +0100

    Ejercicio 20. Borrado múltiple de películas

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmListScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmListScreen.kt
@@ -1,18 +1,24 @@
 package com.campusdigitalfp.filmoteca.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -24,11 +30,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -40,18 +49,18 @@ import com.campusdigitalfp.filmoteca.sampledata.FilmDataSource
 @Composable
 fun FilmListScreen(navController: NavHostController) {
 
-    val filmList = FilmDataSource.films
-    val film = filmList.getOrNull(1)
-
+    val filmList = remember { FilmDataSource.films }
 
     var expanded by remember { mutableStateOf(false) }
-
+    val selectedFilms = remember { mutableStateOf(mutableSetOf<Int>()) }
+    var isActionMode by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
             TopAppBar(
-
-                title = { Text(text = "Filmoteca") },
+                title = {
+                    Text(text = "Filmoteca")
+                },
                 navigationIcon = {
                     Box(
                         modifier = Modifier.clickable {
@@ -65,82 +74,143 @@ fun FilmListScreen(navController: NavHostController) {
                         )
                     }
                 },
-                //al tener actions no he sabido usar el componente
                 actions = {
-                    IconButton(onClick = { expanded = !expanded }) {
-                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Menú")
-                    }
+                    if (isActionMode) {
+                        IconButton(onClick = {
+                            val itemsToRemove = FilmDataSource.films.filterIndexed { index, _ -> selectedFilms.value.contains(index) }
+                            FilmDataSource.films.removeAll(itemsToRemove)
+                            selectedFilms.value.clear()
+                            isActionMode = false
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Delete,
+                                contentDescription = "Eliminar seleccionados"
+                            )
+                            navController.navigate("FilmListScreen")
+                            navController.popBackStack()
+                        }
+                    } else {
+                        IconButton(onClick = { expanded = !expanded }) {
+                            Icon(imageVector = Icons.Default.Menu, contentDescription = "Menú")
+                        }
 
-                    // MENU DESPLEGABLE
-                    DropdownMenu(
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false }
-                    ) {
-                        // AÑADIR PELICULA
-                        DropdownMenuItem(
-                            text = { Text("Añadir película") },
-                            onClick = {
-                                expanded = false
-                                val nuevaPelicula = Film(
-                                    id = filmList.size,
-                                    title = "Película por defecto",
-                                    imageResId = R.drawable.pelicula_por_defecto,
-                                    director = "Director desconocido",
-                                    year = 2023,
-                                    genre = Film.GENRE_ACTION,
-                                    format = Film.FORMAT_DIGITAL,
-                                    imdbUrl = "https://www.imdb.com",
-                                    comments = ""
-                                )
-                                filmList.add(nuevaPelicula)
-                                navController.navigate("FilmListScreen")
-                                navController.popBackStack()
-                            }
-                        )
-
-                        // ACERCA DE
-                        DropdownMenuItem(
-                            text = { Text("Acerca de") },
-                            onClick = {
-                                expanded = false
-                                navController.navigate("AboutScreen")
-                            }
-                        )
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            DropdownMenuItem(
+                                text = { Text("Añadir película") },
+                                onClick = {
+                                    expanded = false
+                                    val nuevaPelicula = Film(
+                                        id = filmList.size,
+                                        title = "Película por defecto",
+                                        imageResId = R.drawable.pelicula_por_defecto,
+                                        director = "Director desconocido",
+                                        year = 2023,
+                                        genre = Film.GENRE_ACTION,
+                                        format = Film.FORMAT_DIGITAL,
+                                        imdbUrl = "https://www.imdb.com",
+                                        comments = ""
+                                    )
+                                    filmList.add(nuevaPelicula)
+                                    navController.navigate("FilmListScreen")
+                                    navController.popBackStack()
+                                }
+                            )
+                            DropdownMenuItem(
+                                text = { Text("Acerca de") },
+                                onClick = {
+                                    expanded = false
+                                    navController.navigate("AboutScreen")
+                                }
+                            )
+                        }
                     }
                 }
-
             )
         }
     ) {
-
         LazyColumn(
             modifier = Modifier
-                .padding(it) // Aquí `it` es un PaddingValues de Scaffol, por eso sustituyo el 16.dp
+                .padding(it)
                 .fillMaxSize()
         ) {
-
             itemsIndexed(filmList) { index, film ->
-                VistaFilm(film, index, navController)
+                VistaFilm(
+                    film = film,
+                    index = index,
+                    navController = navController,
+                    isSelectedMode = selectedFilms.value.isNotEmpty(),
+                    onLongClick = {
+                        isActionMode = true
+                        selectedFilms.value.add(index)
+                    },
+                    selectedFilms = selectedFilms,
+                    onClick = {
+                        if (isActionMode) {
+                            // Si estamos en Action Mode, seleccionamos o deseleccionamos el elemento
+                            if (selectedFilms.value.contains(index)) {
+                                selectedFilms.value.remove(index)
+                                if (selectedFilms.value.isEmpty()) isActionMode = false
+                            } else {
+                                selectedFilms.value.add(index)
+                            }
+                        } else {
+                            // Si no estamos en Action Mode, navegar al detalle
+                            navController.navigate("FilmDataScreen/$index")
+                        }
+                    }
+
+                )
             }
         }
-
     }
-
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun VistaFilm(film: Film, index: Int, navController: NavHostController) {
-
-    Row(modifier = Modifier
-        .padding(all = 8.dp)
-        .clickable { navController.navigate("FilmDataScreen/$index") }) {
+fun VistaFilm(
+    film: Film,
+    index: Int,
+    navController: NavHostController,
+    isSelectedMode: Boolean,
+    onLongClick: () -> Unit,
+    selectedFilms: MutableState<MutableSet<Int>>,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+            .background(
+                if (selectedFilms.value.contains(index)) Color.LightGray
+                else MaterialTheme.colorScheme.background
+            )
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = onLongClick
+            )
+    ) {
         Image(
-            painter = painterResource(film.imageResId),
+            painter = painterResource(
+                if (selectedFilms.value.contains(index))
+                    R.drawable.check_selected
+                else film.imageResId
+            ),
             contentDescription = "${film.title}",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier
+                .size(80.dp)
+                .then(
+                    if (selectedFilms.value.contains(index)) {
+                        Modifier
+                            .clip(CircleShape)
+                    } else Modifier
+                )
         )
 
-        Column {
+
+        Column(modifier = Modifier.padding(start = 8.dp)) {
             Text(
                 text = film.title ?: "Sin título",
                 style = MaterialTheme.typography.bodyLarge
@@ -150,6 +220,5 @@ fun VistaFilm(film: Film, index: Int, navController: NavHostController) {
                 style = MaterialTheme.typography.bodySmall
             )
         }
-
     }
 }
